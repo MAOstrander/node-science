2648e9f04707e27f47700ab073f66e2c2ae77afb
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-07 14:41:07 -0600

    creating arrays from scratch

---
+++ list.js
@@ -0,0 +1,34 @@
+'use strict'
+
+class Node {
+  constructor (data) {
+    this.payload = data
+    this.next = null
+  }
+}
+
+class List {
+  constructor () {
+    this.head = null
+    this.tail = null
+  }
+
+  add (node) {
+    if (this.head) {
+      this.tail.next = node
+      this.tail = node
+    } else {
+      this.head = node
+      this.tail = node
+    }
+  }
+}
+
+const list = new List
+list.add(new Node('A'))
+list.add(new Node('B'))
+list.add(new Node('C'))
+list.add(new Node('D'))
+list.add(new Node('E'))
+list.add(new Node('F'))
+console.log(list)
